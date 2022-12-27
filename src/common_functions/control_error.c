535dd02dd694c2061c7adbc86a22d612f713f0e8
carrera29 <99269629+carrera29@users.noreply.github.com>
2022-12-27 21:07:25 +0100

    Update control_error.c

--- src/common_functions/control_error.c
+++ src/common_functions/control_error.c
@@ -12,6 +12,18 @@
 
 #include "so_long.h"
 
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] && s1[i] == s2[i] && i < ((unsigned int)n - 1))
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+	
 void	clean_map(t_data *data)
 {
 	int	y;
